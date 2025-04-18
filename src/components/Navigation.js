24a205e77b9e32c90073fda47a3f132b59832e96
aycher2000 <aycher@gmail.com>
2025-04-18 14:34:01 -0700

    Add logo to navigation header with styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@/styles/Navigation.module.css';
 
 export default function Navigation() {
   return (
     <nav className={styles.nav}>
       <div className={styles.navContainer}>
-        <Link href="/" className={styles.navLink}>Home</Link>
-        <Link href="/about" className={styles.navLink}>About</Link>
-        <Link href="/contact" className={styles.navLink}>Contact</Link>
+        <div className={styles.logoContainer}>
+          <Image 
+            src="/images/wildwesttech-logogold.png"
+            alt="Wild West Tech Logo"
+            width={40}
+            height={40}
+            style={{
+              width: '40px',
+              height: '40px',
+            }}
+          />
+        </div>
+        <div className={styles.links}>
+          <Link href="/" className={styles.navLink}>Home</Link>
+          <Link href="/about" className={styles.navLink}>About</Link>
+          <Link href="/contact" className={styles.navLink}>Contact</Link>
+        </div>
       </div>
     </nav>
   );
