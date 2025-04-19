92aabfec675a43595e33ee61cc03831be549da6c
aycher2000 <aycher@gmail.com>
2025-04-19 11:24:40 -0700

    Add secondary navigation links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,10 @@ import Link from 'next/link';
 import styles from '@/styles/Footer.module.css';
 
 export default function Footer() {
+  const handleDummyClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -9,6 +13,11 @@ export default function Footer() {
           <Link href="/" className={styles.link}>Home</Link>
           <Link href="/about" className={styles.link}>About</Link>
           <Link href="/contact" className={styles.link}>Contact</Link>
+        </div>
+        <div className={styles.links}>
+          <Link href="#" onClick={handleDummyClick} className={styles.link}>Services</Link>
+          <Link href="#" onClick={handleDummyClick} className={styles.link}>FAQ</Link>
+          <Link href="#" onClick={handleDummyClick} className={styles.link}>Blog</Link>
         </div>
         <div className={styles.copyright}>
           © 2025 Wild West Tech. All rights reserved.
