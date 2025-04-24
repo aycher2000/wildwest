2e06587323584ba2dd4ea265c884e5987c4cbadf
aycher2000 <aycher@gmail.com>
2025-04-24 06:19:42 -0700

    Update sitemap configuration and regenerate sitemap files

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.wildwesttech.pro',
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: './public',
+} 
